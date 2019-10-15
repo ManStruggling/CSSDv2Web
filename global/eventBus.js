21979f31c992f46fd1c14f10eaa7fa01d484a91e
朱志林 <1343842690@qq.com>
2019-10-15 05:01:22 +0000

    Deleted global

--- global/eventBus.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue'
-let eventBus = new Vue();
-export default eventBus;
--- global/public.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery'
-$('body').on('click', '.basic_table_tb_tr_box', function() {
-    $(this).parent().toggleClass('isActive');
-})
-
-$('body').on('click', '.cssd_table_expand .el-table__row', function(e) {
-    // $(this).next().find('td').slideToggle('fast');
-    $(this).next().stop().slideToggle('fast');
-})
