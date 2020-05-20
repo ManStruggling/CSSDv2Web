b9f91dcd4f9571fbcdfccf457a19e8877b05b17f
Soulfangli <854150653@qq.com>
2020-05-20 10:04:45 +0800

    打印回收清单

--- src/components/recovery/Record.vue
+++ src/components/recovery/Record.vue
@@ -67,6 +67,7 @@
                   @click.stop="changeRecycleRecord(collapseIndex)"
                   :disabled="(UserInfo.JobAndCompetence.includes('111')||UserInfo.JobAndCompetence.includes('000')||UserInfo.JobAndCompetence.includes('100'))&&!item.CanNotBeModified?false:true"
                 >修改</el-button>
+                <el-button size="mini" @click.stop="handlePrint(item.Id)">打印回收清单</el-button>
               </p>
             </div>
           </div>
@@ -188,6 +189,25 @@ export default {
           recordId: this.recordList[index].Id
         }
       });
+    },
+    //打印回收清单
+    handlePrint(Id) {
+      axios({
+        url: `/api/Recycle/PrintRecycleList?recycleId=${Id}`
+      })
+        .then(res => {
+          if (res.data.Code == 200 && res.data.Data) {
+            res.data.Data.forEach(element => {
+              CSManager.PrintBarcode(JSON.stringify(element));
+            });
+          } else {
+            this.showInformation({
+              classify: "message",
+              msg: res.data.Msg
+            });
+          }
+        })
+        .catch(err => {});
     },
     searchRecordsData() {
       this.GLOBAL.searchRecord(
--- src/components/recovery/Registration.vue
+++ src/components/recovery/Registration.vue
@@ -621,6 +621,11 @@ export default {
                     return console.error(err);
                   });
               }
+              if (res.data.Data && Array.isArray(res.data.Data)) {
+                res.data.Data.forEach(element => {
+                  CSManager.PrintBarcode(JSON.stringify(element));
+                });
+              }
               if (this.recoveryRecordModle) {
                 this.$router.push({
                   path: "/recovery/record",
